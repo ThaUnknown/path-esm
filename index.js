1b60e021b3ef2fb7c3b1385666c9e54de7d6f5ad
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-06-16 12:22:59 +0200

    fix: import shaking

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import * as path from './lib/index.js'
+export const _makeLong = a => a
+export const sep = '/'
+export const delimiter = ':'
 
-export default {
-  ...path,
-  posix: path,
-  win32: path
-}
+export { default as basename } from './lib/basename.js'
+export { default as dirname } from './lib/dirname.js'
+export { default as extname } from './lib/extname.js'
+export { default as format } from './lib/format.js'
+export { default as isAbsolute } from './lib/isAbsolute.js'
+export { default as join } from './lib/join.js'
+export { default as normalize } from './lib/normalize.js'
+export { default as parse } from './lib/parse.js'
+export { default as relative } from './lib/relative.js'
+export { default as resolve } from './lib/resolve.js'
+
+export * as default from './index.js'
+export * as win32 from './index.js'
+export * as posix from './index.js'
